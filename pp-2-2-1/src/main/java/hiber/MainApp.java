6aa259c476bbd1898468e8d6c40c93cc993dab90
Lansetnick <118694414+Lansetnick@users.noreply.github.com>
2023-07-06 16:07:00 +0500

    Add files via upload

---
+++ pp-2-2-1/src/main/java/hiber/MainApp.java
@@ -0,0 +1,32 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+
+      String model = "Model1";
+      int i = 1;
+      List<User> users1 = userService.getUserByCar(model, i);
+      for (User user : users1) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+      context.close();
+   }
+}
